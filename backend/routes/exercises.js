872d1062f051ac7ed688cbf53fd31210b7ce3f77
Diego Andino <andino.diego98@gmail.com>
2020-08-07 19:31:50 -0600

    Basic React NavTabs done

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-let exercise = require('../models/exercise.model');
 const Exercise = require('../models/exercise.model');
+
+// This model will change
+let exercise = require('../models/exercise.model');
 
 // GET REQUEST
 router.route('/').get((req,res) => {
@@ -26,6 +28,38 @@ router.route('/add').post((req,res) => {
     // Save to MongoDB
     newExercise.save()
     .then(() => res.json('New Exercise Added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// GET BY ID
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error:' + err));
+});
+
+// DELETE REQUEST BY ID
+router.route('/:id').delete((req,res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Exercise item deleted'))
+    .catch(err => res.status(400).json('Error:' + err));
+});
+
+// PUT REQUEST BY ID
+router.route('/update/:id').post((req,res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.duration = Number(req.body.duration);
+        exercise.date = Date.parse(req.body.date);
+
+        // Update in MongoDB
+        exercise.save()
+        .then(() => res.json('Exercise Updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
